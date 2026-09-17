33682e3e0a7ea66463fd9cdf67aa934c242093e9
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-17 22:39:18 +0100

    refactor: centralize database bootstrap in App

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { useGame } from "./state/GameStore.js";
-
-// carregar dados iniciais da BD (JSON em public/data ou API)
-useGame.getState().loadData();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
