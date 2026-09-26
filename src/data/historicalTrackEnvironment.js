0f8b706d76039fadd5a8a07d99e4e209b027b840
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-26 15:30:41 +0100

    Track 2.1A.3: expose historical environment metadata

---
+++ src/data/historicalTrackEnvironment.js
@@ -0,0 +1,12 @@
+export const HISTORICAL_TRACK_ENVIRONMENTS=Object.freeze({
+  tr_0018_provisional:Object.freeze({
+    asset:"/tracks/historical/buenos-aires-no15-1980.svg",
+    presentation_only:true,
+    reference_year:1980,
+    layout:"Circuit No. 15",
+    start_finish_location:"upper_straight",
+    start_finish_direction:"right"
+  })
+});
+
+export default HISTORICAL_TRACK_ENVIRONMENTS;
