1c6c32c4be82ef03cc7243aaeb92fdc6e272baea
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-26 17:28:39 +0100

    Track 2.1A.4: register Argentina intel colours

--- src/data/historicalTrackEnvironment.js
+++ src/data/historicalTrackEnvironment.js
@@ -1,12 +1,14 @@
 export const HISTORICAL_TRACK_ENVIRONMENTS=Object.freeze({
   tr_0018_provisional:Object.freeze({
     asset:"/tracks/historical/buenos-aires-no15-1980.svg",
-    view_box:[0,0,1642,958],
+    view_box:[0,0,1649,954],
     presentation_only:true,
     reference_year:1980,
     layout:"Circuit No. 15",
     start_finish_location:"upper_straight",
-    start_finish_direction:"right"
+    start_finish_direction:"right",
+    sector_colors:["#ef4444","#22d3ee","#facc15"],
+    pit_lane_color:"#2563eb"
   })
 });
 
