ae04dff0dc75637303742450aa9dc1427b90705a
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-26 18:15:56 +0100

    Hotfix: import Save World career experience in tests

--- tests/driverLifecycle.test.js
+++ tests/driverLifecycle.test.js
@@ -1,6 +1,7 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import {
+  driverCareerExperience,
   driverLifecycleSnapshot,
   driverMonthlyCareerDevelopmentPlan,
   driverPreviousMonthPerformance,
